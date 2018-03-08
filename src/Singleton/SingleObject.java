71ed2db34463529460152a99186a4500d9f491a5
dani832m <31951102+dani832m@users.noreply.github.com>
2018-03-08 09:30:50 +0100

    Mediator design pattern

--- src/Singleton/SingleObject.java
+++ src/Singleton/SingleObject.java
@@ -6,14 +6,14 @@ public class SingleObject {
     private static SingleObject instance = new SingleObject();
 
     //Make the constructor private so that this class cannot be instantiated
-    private SingleObject(){}
+    private SingleObject() {}
 
     //Get the only object available
-    public static SingleObject getInstance(){
+    public static SingleObject getInstance() {
         return instance;
     }
 
-    public void showMessage(){
+    public void showMessage() {
         System.out.println("Hello Mothafukka!");
     }
 
